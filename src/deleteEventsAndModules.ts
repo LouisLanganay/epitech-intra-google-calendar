f1a029c73784c88d6508190fc95bf12fc7e461ca
LouisLanganay <louis.langanay@epitech.eu>
2024-01-02 11:42:19 +0100

    fix: Filter out events in the past when deleting events and modules

--- src/deleteEventsAndModules.ts
+++ src/deleteEventsAndModules.ts
@@ -3,6 +3,7 @@ import {
   CalendarEvent,
   Planning
 } from './utils/types';
+import moment from 'moment';
 
 async function deleteEventsAndModules(
   planning: Planning,
@@ -16,9 +17,15 @@ async function deleteEventsAndModules(
     return 'm' + module.id.toString();
   });
 
-  const eventsToDelete = eventList.filter(({id, status}) => {
-    return (!eventsIds.includes(id) &&
-      !modulesIds.includes(id)) && status === 'confirmed';
+  const eventsToDelete = eventList.filter(({ id, status, end }) => {
+    const isEventInPast = moment(end.dateTime).isBefore(moment());
+
+    return (
+      !eventsIds.includes(id) &&
+      !modulesIds.includes(id) &&
+      status === 'confirmed' &&
+      !isEventInPast
+    );
   });
 
   console.info('Deleting events...');
